5d3818f822a2370b82e2d99afa80d64e4ffc7054
TimothyBear11 <timothybear1183@pm.me>
2025-12-31 13:23:10 -0500

    added Ambxst

--- home.nix
+++ home.nix
@@ -11,15 +11,14 @@ let
     qtile = "qtile";
     noctalia = "noctalia";
     mango = "mango";
-
+    # Add Ambxst here (Capitalized A to match ~/.config/Ambxst)
+    Ambxst = "Ambxst";
   };
 in
 {
-
   imports = [
     ./terminal.nix
     ./apps.nix
-
   ];
 
   home.username = "tbear";
@@ -27,18 +26,19 @@ in
   home.stateVersion = "25.05";
 
   programs.home-manager.enable = true;
-
   services.mako.enable = false;
 
   home.packages = with pkgs; [
     gcc
+    # Add Ambxst if you added it to your flake.nix inputs
+    # inputs.ambxst.packages.${pkgs.system}.default
+
     (pkgs.writeShellApplication {
       name = "ns";
       runtimeInputs = with pkgs; [
         fzf
         (nix-search-tv.overrideAttrs (oldAttrs: {
           env.GOEXPERIMENT = "jsonv2";
-          # You need to clear disallowedReferences to allow the Go compiler in the closure
           disallowedReferences = [ ];
           disallowedRequisites = [ ];
         }))
@@ -49,10 +49,16 @@ in
     inputs.zen-browser.packages.${pkgs.system}.default
   ];
 
+  # 1. Standard Configs (~/.config/...)
   xdg.configFile = builtins.mapAttrs
     (name: subpath: {
       source = create_symlink "${dotfiles}/${subpath}";
       recursive = true;
     })
     configs;
+
+  # 2. Data Files (~/.local/share/...)
+  # This handles the specific wallpaper fix we found
+  xdg.dataFile."Ambxst/wallpapers.json".source =
+    create_symlink "${dotfiles}/Ambxst/wallpapers.json";
 }
