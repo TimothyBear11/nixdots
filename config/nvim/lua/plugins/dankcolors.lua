c1ec7bf65206f4ea28ffd4c575bc59a28cd50027
TimothyBear11 <timothybear1183@pm.me>
2026-01-29 19:33:50 -0500

    end of night updates

--- config/nvim/lua/plugins/dankcolors.lua
+++ config/nvim/lua/plugins/dankcolors.lua
@@ -4,22 +4,22 @@ return {
 		priority = 1000,
 		config = function()
 			require('base16-colorscheme').setup({
-				base00 = '#121318',
-				base01 = '#121318',
+				base00 = '#0e1514',
+				base01 = '#0e1514',
 				base02 = '#5c6370',
 				base03 = '#5c6370',
 				base04 = '#abb2bf',
 				base05 = '#ffffff',
 				base06 = '#ffffff',
 				base07 = '#ffffff',
-				base08 = '#e0625f',
-				base09 = '#e0625f',
-				base0A = '#c0d3ff',
-				base0B = '#86e086',
-				base0C = '#6f73b7',
-				base0D = '#c0d3ff',
-				base0E = '#5679b2',
-				base0F = '#5679b2',
+				base08 = '#e05f68',
+				base09 = '#e05f68',
+				base0A = '#89e3e4',
+				base0B = '#8ee086',
+				base0C = '#4f8189',
+				base0D = '#89e3e4',
+				base0E = '#388577',
+				base0F = '#388577',
 			})
 
 			vim.api.nvim_set_hl(0, 'Visual', {
@@ -28,14 +28,14 @@ return {
 				bold = true
 			})
 			vim.api.nvim_set_hl(0, 'Statusline', {
-				bg = '#c0d3ff',
-				fg = '#121318',
+				bg = '#89e3e4',
+				fg = '#0e1514',
 			})
 			vim.api.nvim_set_hl(0, 'LineNr', { fg = '#5c6370' })
-			vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#6f73b7', bold = true })
+			vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#4f8189', bold = true })
 
 			vim.api.nvim_set_hl(0, 'Statement', {
-				fg = '#5679b2',
+				fg = '#388577',
 				bold = true
 			})
 			vim.api.nvim_set_hl(0, 'Keyword', { link = 'Statement' })
@@ -43,24 +43,24 @@ return {
 			vim.api.nvim_set_hl(0, 'Conditional', { link = 'Statement' })
 
 			vim.api.nvim_set_hl(0, 'Function', {
-				fg = '#c0d3ff',
+				fg = '#89e3e4',
 				bold = true
 			})
 			vim.api.nvim_set_hl(0, 'Macro', {
-				fg = '#c0d3ff',
+				fg = '#89e3e4',
 				italic = true
 			})
 			vim.api.nvim_set_hl(0, '@function.macro', { link = 'Macro' })
 
 			vim.api.nvim_set_hl(0, 'Type', {
-				fg = '#6f73b7',
+				fg = '#4f8189',
 				bold = true,
 				italic = true
 			})
 			vim.api.nvim_set_hl(0, 'Structure', { link = 'Type' })
 
 			vim.api.nvim_set_hl(0, 'String', {
-				fg = '#86e086',
+				fg = '#8ee086',
 				italic = true
 			})
 
